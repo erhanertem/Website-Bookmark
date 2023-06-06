38a2b526a3ba8b1cc44a12a28da4a5c2b16c62a1
Erhan Ertem <eertem@gmail.com>
2023-06-06 17:10:03 +0300

    Add hamburger menu and toggle JS

--- js/script.js
+++ js/script.js
@@ -33,3 +33,22 @@ function onTabClick(event) {
 		.getElementsByClassName(classString)[0]
 		.classList.remove('hidden');
 }
+
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
+
+// Hamburger Button Listener
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+	btn.classList.toggle('open');
+	menu.classList.toggle('flex');
+	menu.classList.toggle('hidden');
+
+	if (menu.classList.contains('flex')) {
+		logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+	} else {
+		logo.setAttribute('src', './images/logo-bookmark.svg');
+	}
+}
